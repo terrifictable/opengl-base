4a92bbeb1aa1178438a99571ba72430bfec6ed96
TerrificTable <omegagaming189@gmail.com>
2023-06-18 23:08:44 +0200

    fixed GL_INVALID_OPERATION error when linking vao

--- src/app/gl/vao/vao.cpp
+++ src/app/gl/vao/vao.cpp
@@ -15,12 +15,14 @@ VAO::~VAO() {
 void VAO::link(std::unique_ptr<ngl::VBO> &buffer_object, GLuint layout, GLuint components_count, GLenum type, GLsizeiptr stride, void* offset) {
     glEnableVertexAttribArray(layout);
     buffer_object->bind();
+    this->bind();
 
     glVertexAttribPointer(layout, components_count, type, GL_FALSE, stride, offset);
 }
 
 void VAO::unlink(std::unique_ptr<ngl::VBO> &buffer_object, GLuint layout) {
     buffer_object->unbind();
+    this->unbind();
     glDisableVertexAttribArray(layout);
 }
 
